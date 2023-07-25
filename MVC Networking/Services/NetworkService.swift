a772b5a77c0710fe428690a5ad31d915c3518de1
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-25 11:22:45 +0700

    :newspaper: create network service

---
+++ MVC Networking/Services/NetworkService.swift
@@ -0,0 +1,20 @@
+//
+//  NetworkService.swift
+//  MVC Networking
+//
+//  Created by Yusuf Saifudin on 25/07/23.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkService {
+    
+    func fetchPlaces( result: @escaping (DataResponse<[PlaceModel], AFError>) -> Void) {
+        let url = "https://info-malang-batu.firebaseapp.com/List_place_malang_batu.json"
+        AF.request(url, method: .get).responseDecodable(of: [PlaceModel].self) { response in
+            result(response)
+        }
+    }
+        
+}
