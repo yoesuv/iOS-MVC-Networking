42b18af4bfc109b44d4e24f761a1d78b327e082d
Yusuf Saifudin <yusuf.saifudin@gmail.com>
2023-07-25 14:40:38 +0700

    :newspaper: cell place

---
+++ MVC Networking/PlaceCell.swift
@@ -0,0 +1,23 @@
+//
+//  PlaceCell.swift
+//  MVC Networking
+//
+//  Created by Yusuf Saifudin on 25/07/23.
+//
+
+import UIKit
+
+class PlaceCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
